7f214bfb66d9f2e50ed90043dd329209522abb1c
Alan Cook <leolamb3@ai678.top>
2026-09-01 18:45:55 +0100

    Create __init__.py

---
+++ ai-projects/llama_index/llama-index-integrations/embeddings/llama-index-embeddings-huggingface-openvino/llama_index/embeddings/huggingface_openvino/__init__.py
@@ -0,0 +1,12 @@
+from llama_index.embeddings.huggingface_openvino.base import (
+
+    OpenVINOEmbedding,
+
+    OpenVINOClipEmbedding,
+
+)
+
+
+
+__all__ = ["OpenVINOEmbedding", "OpenVINOClipEmbedding"]
+
